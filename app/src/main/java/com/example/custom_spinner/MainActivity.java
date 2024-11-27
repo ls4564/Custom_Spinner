ed4fc25252b7d13f42742cfd4683492284d19718
Lior1254 <liorshemtov2008@gmail.com>
2024-11-27 09:09:16 +0200

    add arr_pull

--- app/src/main/java/com/example/custom_spinner/MainActivity.java
+++ app/src/main/java/com/example/custom_spinner/MainActivity.java
@@ -11,12 +11,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Countries[] countries_arr;
+    Countries cac;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         weddings();
+        countries_arr = new Countries[7];
+        full_arr();
+
+
 
 
     }
@@ -24,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
     public void weddings()
     {
         //customAdapter adp = new customAdapter(this,arr_data);
+    }
+
+    public void full_arr()
+    {
+        for(int i = 0 ; i < countries_arr.length ; i++)
+        {
+            countries_arr[i].setCountryName(getResources().getStringArray(R.array.countryName)[i]);
+            countries_arr[i].setCountryCity(getResources().getStringArray(R.array.countryCity)[i]);
+            countries_arr[i].setCountryName(getResources().getStringArray(R.array.countryCityPopulation)[i]);
+        }
     }
 }
